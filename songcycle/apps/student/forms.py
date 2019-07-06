d59f5326b697ff346061093c9e9ffd1557bd1398
TsJazz27Sumin <ts.jazz.27.sumin@gmail.com>
2019-07-06 14:49:26 +0900

    不要なクラスを削除した。 タイトルを現状に合わせた。

--- songcycle/apps/student/forms.py
+++ songcycle/apps/student/forms.py
@@ -1,12 +1,4 @@
-from django.contrib.auth import forms as auth_forms
 from django import forms
-
-class LoginForm(auth_forms.AuthenticationForm):
-    
-    def __init__(self, *args, **kw):
-        super().__init__(*args, **kw)
-        for field in self.fields.values():
-            field.widget.attrs['placeholder'] = field.label
 
 class RequestLoginForm(forms.Form):
     
