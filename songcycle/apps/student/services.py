83604821c8fb32fd2f59cb3c8f6395eeac55a4c9
TsJazz27Sumin <ts.jazz.27.sumin@gmail.com>
2019-07-06 14:35:17 +0900

    home画面にワンタイムパスワード付きでログインされる想定。

--- songcycle/apps/student/services.py
+++ songcycle/apps/student/services.py
@@ -1,4 +1,11 @@
 # import area
 
-def is_exist_email(email):
+def exist_email(email):
     return True
+
+def exist_onetime_password(onetime_password):
+    # TODO DBとの照合
+    if(onetime_password is not None and onetime_password == "abc"):
+        return True
+    
+    return False
--- songcycle/apps/student/views.py
+++ songcycle/apps/student/views.py
@@ -11,7 +11,19 @@ from django.contrib import messages
 
 # def エリア
 
-def home(request):                             
+def home(request):
+    # こんなURLでアクセスされる想定。
+    # http://127.0.0.1:8000/student/home/?onetimepassword=abc
+    onetime_password = request.GET.get("onetimepassword")
+
+    # TODO
+    # テンポラリーのログインURL情報がDBにあるので照合する。
+    if(services.exist_onetime_password(onetime_password)):
+        # OKだったら、セッションにユーザ情報を登録する。
+        return render(request, 'student/home.html')
+
+    print("exist_onetime_password is false")
+    # NGだったら、テンポラリーのログインURLが無効になっていることを伝える。
     return render(request, 'student/home.html')
 
 # class エリア
@@ -26,14 +38,18 @@ class requestLoginView(FormView):
         # TODO 
         # メールアドレスが先生が登録済みのものかエラーチェックを追加する。
 
-        if(services.is_exist_email("mailaddress")):
+        if(services.exist_email("mailaddress")):
             print("is_exist_email is true")
+
+            # TODO 
             # OKだったら、テンポラリーのログインURLを送信する。
-            # DBにテンポラリーのログインURLをどのアドレスに送信したかを登録しておく。
+            # DBにテンポラリーのログインURL情報をどのアドレスに送信したかを登録しておく。
             # もちろん有効期限付きで15分以内とか。
             # テンポラリーのデータは、1日経ったらcronで消したい。
         else:
             print("is_exist_email is false")
+
+            # TODO 
             # NGだったら、できればWarning出してログを登録して監視したい。
         
         return render(self.request, 'student/request_login_success.html', {})
