8ddfbb5f225435927ea16346449c73d8275f1064
TsJazz27Sumin <ts.jazz.27.sumin@gmail.com>
2019-07-06 11:58:33 +0900

    漏れると困る設定は、gitignore

--- songcycle/config/settings/base.py
+++ songcycle/config/settings/base.py
@@ -75,22 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'songcycle',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'POST': '5432'
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
--- songcycle/config/settings/develop.py
+++ songcycle/config/settings/develop.py
@@ -1,1 +1,17 @@
 from .base import *
+
+
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'songcycle',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'POST': '5432'
+    }
+}
