0d6c9f6add6df7485729037c4a10028c8c0c8523
TsJazz27Sumin <ts.jazz.27.sumin@gmail.com>
2019-07-06 13:26:44 +0900

    formsのvalidと絡めた処理を作成中。

--- songcycle/apps/student/urls.py
+++ songcycle/apps/student/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('login/', views.loginView.as_view(), name="login"),
     path('request-login/', views.requestLoginView.as_view(), name="request_login"),
-    path('index/',views.index, name="index"),
-    path('request-login/success/',views.request_login_success, name="request_login_success")
+    path('home/',views.home, name="home")
 ]
--- songcycle/apps/student/views.py
+++ songcycle/apps/student/views.py
@@ -6,21 +6,34 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views.generic import FormView
 from django.urls import reverse_lazy
+from django.contrib import messages
 
-def index(request):                             
-    return render(request, 'student/index.html')
+# def エリア
 
-def request_login_success(request):                             
-    return render(request, 'student/request_login_success.html')
+def home(request):                             
+    return render(request, 'student/home.html')
 
-class loginView(LoginView):
-    form_class = forms.LoginForm
-    template_name = "student/login.html"
+# class エリア
 
 class requestLoginView(FormView):
     form_class = forms.RequestLoginForm
     template_name = "student/request_login.html"
-    success_url = reverse_lazy("request_login_success")
+
+    def form_valid(self, form):
+        print("form_valid called!")
+        # TODO メールアドレスが先生が登録済みのものかエラーチェックを追加する。
+        # OKだったら、テンポラリーのログインURLを送信する。
+        return render(self.request, 'student/request_login_success.html', {})
+    
+    def form_invalid(self, form):
+        print("form_invalid called!")
+        # 失敗してもログインURLを送信したことにする。
+        # TODO できればWarning出してログ監視したい。
+        return render(self.request, 'student/request_login_success.html', {})
+
+class loginView(LoginView):
+    form_class = forms.LoginForm
+    template_name = "student/login.html"
 
 # TODO ログアウトできてない疑惑
 class logoutView(LoginRequiredMixin, LogoutView):
