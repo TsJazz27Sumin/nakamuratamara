0c2316763fa37da2302477ec92c3cc02c3453da8
TsJazz27Sumin <ts.jazz.27.sumin@gmail.com>
2019-07-07 13:03:38 +0900

    localのgitにはコミット

---
+++ songcycle/config/settings/production.py
@@ -0,0 +1,17 @@
+from .base import *
+
+
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'xxx',
+        'USER': 'xxx',
+        'PASSWORD': 'xxx',
+        'HOST': 'xxx',
+        'POST': '5432'
+    }
+}
