cf509afbc584beff8516bc2851714dd828d26b63
TsJazz27Sumin <ts.jazz.27.sumin@gmail.com>
2019-07-09 01:41:38 +0900

    管理者専用のデコレータを用意した。

--- songcycle/apps/student/decorators/decorator.py
+++ songcycle/apps/student/decorators/decorator.py
@@ -8,3 +8,16 @@ def authenticate(function):
             return redirect('request_login')
         return function(*args, **kwargs)
     return wrapper
+
+def authenticate_admin_only(function):
+    def wrapper(*args, **kwargs):
+        if 'authority' not in args[0].session:
+            # 権限が不明な場合は、セッション切れ、もしくは不正アクセスと見なし、強制ログアウト
+            return redirect('request_login')
+        else:
+            if(args[0].session['authority'] == "admin"):
+                return function(*args, **kwargs)        
+        
+        # 管理者じゃない場合は、Home画面に戻す。
+        return redirect('home')
+    return wrapper
