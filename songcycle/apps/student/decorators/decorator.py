af41b28638e64041593cefb0dd1d0d1cc311e328
蓮見洋平 <hasumiyouhei@hasumiyouheinoMacBook-Pro.local>
2019-07-10 18:23:18 +0900

    Loggingを整えた。

--- songcycle/apps/student/decorators/decorator.py
+++ songcycle/apps/student/decorators/decorator.py
@@ -1,11 +1,20 @@
 from django.shortcuts import redirect
 from apps.student.queries.masterquery import MasterQuery
 import logging
+from user_agents import parse
+
+logger = logging.getLogger("student")
 
 def no_authenticate(function_name):
     def __decorator(function):
         def wrapper(*args, **kwargs):
-            print("no_authenticate : " + function_name)
+
+            request = getattr(args[0], 'request', args[0])
+            user_agent = parse(request.META['HTTP_USER_AGENT'])
+            remote_addr = request.META['REMOTE_ADDR']
+
+            logger.info('{} : {} : {}'.format(user_agent, remote_addr, function_name))
+            
             return function(*args, **kwargs)
         return wrapper
     return __decorator
@@ -14,8 +23,12 @@ def no_authenticate(function_name):
 def authenticate(function_name):
     def __decorator(function):
         def wrapper(*args, **kwargs):
-            
-            print("authenticate : " + function_name)
+
+            request = getattr(args[0], 'request', args[0])
+            user_agent = parse(request.META['HTTP_USER_AGENT'])
+            remote_addr = request.META['REMOTE_ADDR']
+
+            logger.info('{} : {} : {}'.format(user_agent, remote_addr, function_name))
 
             if 'authority' not in args[0].session:
                 # 権限が不明な場合は、セッション切れ、もしくは不正アクセスと見なし、強制ログアウト
@@ -28,7 +41,11 @@ def authenticate_admin_only(function_name):
     def __decorator(function):
         def wrapper(*args, **kwargs):
 
-            print("authenticate_admin_only : " + function_name)
+            request = getattr(args[0], 'request', args[0])
+            user_agent = parse(request.META['HTTP_USER_AGENT'])
+            remote_addr = request.META['REMOTE_ADDR']
+
+            logger.info('{} : {} : {}'.format(user_agent, remote_addr, function_name))
 
             if 'authority' not in args[0].session:
                 # 権限が不明な場合は、セッション切れ、もしくは不正アクセスと見なし、強制ログアウト
--- songcycle/config/settings/base.py
+++ songcycle/config/settings/base.py
@@ -136,3 +136,61 @@ MEDIA_ROOT = (
 SESSION_COOKIE_AGE = 3600 # 60分
 SESSION_SAVE_EVERY_REQUEST = True # 1リクエストごとにセッション情報を更新
 
+# ログ設定
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    # TODO:デフォルトの設定はDEBUGフラグで開発・本番の切り替えを行っているが、そもそも別のファイルにするのが主流かつ安全。
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'formatters': {
+        'django.server': {
+            '()': 'django.utils.log.ServerFormatter',
+            'format': '[%(server_time)s] %(message)s a',
+        },
+        'verbose': {
+            'format': '【%(levelname)s 】%(asctime)s : %(module)s : %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler', 
+            'formatter': 'verbose'
+        },
+        'django.server': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'django.server',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'mail_admins'],
+            'level': 'INFO',
+        },
+        'django.server': {
+            'handlers': ['django.server'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        #TODO:ファイル出力
+        'student': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    }
+}
