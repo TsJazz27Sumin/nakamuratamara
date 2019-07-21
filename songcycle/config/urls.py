aecfd7c5c0a9b09e527d352f3567f2675ccbdc49
蓮見洋平 <hasumiyouhei@hasumiyouheinoMacBook-Pro.local>
2019-07-21 21:56:07 +0900

    style調整とfaviconを追加した。

--- songcycle/config/urls.py
+++ songcycle/config/urls.py
@@ -13,16 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path('student/', include('student.urls')),
+    path('favicon.ico', RedirectView.as_view(url='/static/images/favicon.ico'))
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
