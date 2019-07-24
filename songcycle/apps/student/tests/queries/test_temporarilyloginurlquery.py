9d017e41b71c8ea82e45a6f7f460bd1c82f0e158
蓮見洋平 <hasumiyouhei@hasumiyouheinoMacBook-Pro.local>
2019-07-25 00:50:35 +0900

    TemporarilyLoginUrlQueryTestCaseを整備した。

---
+++ songcycle/apps/student/tests/queries/test_temporarilyloginurlquery.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from datetime import datetime, timedelta
+
+from apps.student.queries.temporarilyloginurlquery import TemporarilyLoginUrlQuery
+from apps.student.models.temporarilyloginurl import TemporarilyLoginUrl
+
+
+class TemporarilyLoginUrlQueryTestCase(TestCase):
+
+    def setUp(self):
+        TemporarilyLoginUrl.objects.create(onetime_password='pwd')
+
+    def test_is_valid_onetime_password_case1(self):
+
+        valid_time = datetime.now() - timedelta(minutes=15)
+    
+        result = TemporarilyLoginUrlQuery().is_valid_onetime_password('pwd', valid_time)
+        self.assertEqual(result is not None, True)
+
+    def test_is_valid_onetime_password_case2(self):
+    
+        valid_time = datetime.now() + timedelta(minutes=15)
+    
+        result = TemporarilyLoginUrlQuery().is_valid_onetime_password('pwd', valid_time)
+        self.assertEqual(result is not None, False)
