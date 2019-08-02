b63b494bd57cfd905a18dfc5b5affab4dc73d71f
蓮見洋平 <hasumiyouhei@hasumiyouheinoMacBook-Pro.local>
2019-08-02 16:15:22 +0900

    ReportRepositoryTestCaseを整備した。

--- songcycle/apps/student/tests/repositories/test_reportrepository.py
+++ songcycle/apps/student/tests/repositories/test_reportrepository.py
@@ -14,7 +14,7 @@ class ReportRepositoryTestCase(TestCase):
         google_file_id = "g0001"
         target_year = "2019"
         comment = "comment001"
-        login_user_id = "U0001"
+        login_user_id = "U0000"
 
         ReportRepository().insert(report_id,
                                   auther_user_id,
@@ -27,3 +27,14 @@ class ReportRepositoryTestCase(TestCase):
         report = ReportQuery().get_one(report_id)
 
         self.assertEqual(report.auther_user_id, "U0001")
+        self.assertEqual(report.file_name, "file_name001")
+        self.assertEqual(report.google_file_id, "g0001")
+        self.assertEqual(report.target_year, "2019")
+        self.assertEqual(report.comment, "comment001")
+        self.assertEqual(report.create_user_id, "U0000")
+        self.assertEqual(report.update_user_id, "U0000")
+
+        ReportRepository().delete(report)
+
+        report = ReportQuery().get_one(report_id)
+        self.assertEqual(report, None)
