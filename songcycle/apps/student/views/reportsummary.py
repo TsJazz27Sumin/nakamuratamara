83612ee9f32098cecc40fe67f65f519fe31cd1b9
蓮見洋平 <hasumiyouhei@hasumiyouheinoMacBook-Pro.local>
2019-08-06 00:37:28 +0900

    目次ページのたたき台を作成中。

--- songcycle/apps/student/views/reportsummary.py
+++ songcycle/apps/student/views/reportsummary.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.shortcuts import reverse
-from django.views.generic import FormView
 from user_agents import parse
 from django.http import HttpResponse
 from django.template.loader import render_to_string
 
-from apps.student.forms.reportsummary.loginform import LoginForm
 from apps.student.services.accessinformationservice import AccessInformationService
+from apps.student.queries.reportquery import ReportQuery
 from apps.student.decorators import decorator
 from config.settings.develop import REPORT_SUMMARY_PASSWORD
 
@@ -31,26 +30,42 @@ def index(request):
 
 @decorator.no_authenticate("login")
 def login(request):
-    access_information_service = AccessInformationService()
 
     http_accept_language = request.META['HTTP_ACCEPT_LANGUAGE']
     user_agent = parse(request.META['HTTP_USER_AGENT'])
     remote_addr = request.META['REMOTE_ADDR']
     postparam = request.POST
 
-    targetyear = postparam.get(key="targetyear", default="2019")
+    target_year = postparam.get(key="targetyear", default="2019")
 
     if 'password' in postparam:
         password = postparam["password"]
 
         # TODO：本番と開発環境の切り替え。
         if(REPORT_SUMMARY_PASSWORD == password):
-            access_information_service.add_success(
+            AccessInformationService().add_success(
                 http_accept_language, user_agent, remote_addr, password)
 
-            return render(request, 'student/reportsummary/summary.html', {})
+            result_list = ReportQuery().custom_query(
+                target_year,
+                '',
+                '',
+                0,
+                100,
+                'auther-user-sort',
+                'True')
+
+            context = {
+                'result_list': result_list
+            }
+
+            # TODO：Webとスマホとデザインを分けると思うので、遷移先も分ける。
+            return render(
+                request,
+                'student/reportsummary/summary.html',
+                context=context)
 
     return redirect(
         reverse('report_summary_index') +
         '?targetyear=' +
-        targetyear)
+        target_year)
